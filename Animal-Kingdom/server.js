2247620f620d5d19aa98372ee4681d4a7e3dce44
smackeem <smackeem@outlook.com>
2023-11-21 16:03:01 -0500

    Create schema for pet's medical records.

--- Animal-Kingdom/server.js
+++ Animal-Kingdom/server.js
@@ -3,7 +3,10 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const mongoose = require("mongoose");
+
+const methodOverride = require('method-override');
+require('dotenv').config();
+require('./config/database');
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -19,18 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-// // connect to mongoose
-// mongoose
-//   .connect("mongodb://localhost:27017/yourDatabaseName", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("Connected to MongoDB..."))
-//   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
