cfcbcecd5893f759e67682593464d885f88fd5c9
smackeem <smackeem@outlook.com>
2023-11-21 17:35:27 -0500

    Create new records view to add a new medical record.

--- Animal-Kingdom/controllers/records.js
+++ Animal-Kingdom/controllers/records.js
@@ -1,2 +1,25 @@
 const Record = require('../models/record');
 
+module.exports = {
+    new: newRecord,
+    create,
+};
+
+function newRecord(req, res, next){
+    res.render('records/new',{title: 'New Medical Record'});
+}
+
+async function create(req, res, next){
+    req.body.diagnosis = req.body.diagnosis.trim();
+    req.body.treatment = req.body.treatment.trim();
+    req.body.medication = req.body.medication.trim();
+    let record = await Record.create(req.body);
+    try{
+        record.pet = req.params.id;
+        record.save();
+        res.redirect('/records');
+    }catch(err){
+        console.log(err)
+        res.redirect('/records');
+    }
+}
--- Animal-Kingdom/server.js
+++ Animal-Kingdom/server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride('_method'));
 
 app.use("/", indexRouter);
-app.use("/users", recordsRouter);
+app.use("/records", recordsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
