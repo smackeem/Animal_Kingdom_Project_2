e70c87eb11a7ba4e5ec57bfc51c7fe94ede05793
smackeem <smackeem@outlook.com>
2023-11-25 16:46:19 -0500

    Create views for medical records as a user.

--- Animal-Kingdom/models/appointment.js
+++ Animal-Kingdom/models/appointment.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Schema = mongose.Schema;
+const Schema = mongoose.Schema;
 
 const appointmentSchema = new Schema({
     vet: {type: Schema.Types.ObjectId, ref: 'User'},
