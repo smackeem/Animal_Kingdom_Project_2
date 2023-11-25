279ed95c31cda2480c3226d68d73af065566ed13
smackeem <smackeem@outlook.com>
2023-11-25 18:01:57 -0500

    Create new controller function to render new appointment views.

--- Animal-Kingdom/controllers/appointment.js
+++ Animal-Kingdom/controllers/appointment.js
@@ -4,6 +4,15 @@ const Pet = require('../models/pet');
 const User = require('../models/users');
 
 module.exports = {
+    new: newAppt,
+}
 
+async function newAppt(req, res, next){
+    try{
+        const user = await User.findById(req.params.id);
+        res.render('records/new',{title: 'New Medical Record', errMsg: '', user});
+    }catch(err){
+        console.log(err);
+    }
 }
 
