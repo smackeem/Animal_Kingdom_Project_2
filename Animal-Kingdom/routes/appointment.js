3a054873c293547dcf6054fb247ab96ad75804a5
smackeem <smackeem@outlook.com>
2023-11-25 18:15:35 -0500

    Create routes for rendering and submitting a new appointments form.

--- Animal-Kingdom/routes/appointment.js
+++ Animal-Kingdom/routes/appointment.js
@@ -3,5 +3,9 @@ const router = express.Router();
 
 const appointmentCtrl = require('../controllers/appointment');
 
+router.get('/user/:id/appointments/new', appointmentCtrl.new);
+
+router.post('/user/:id/appointments', appointmentCtrl.create);
+
 
 module.exports = router;
