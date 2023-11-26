cd4968bd335712d750a9b759f92605d645cd6839
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-11-26 13:52:52 -0500

    create appointment route

--- Animal-Kingdom/routes/appointment.js
+++ Animal-Kingdom/routes/appointment.js
@@ -1,7 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const appointmentCtrl = require('../controllers/appointment');
+const appointmentCtrl = require("../controllers/appointment");
 
+router.get("/", appointmentCtrl.index);
+
+router.post("/", appointmentCtrl.create);
+
+router.get("/:id", appointmentCtrl.show);
+
+router.get("/:id/edit", appointmentCtrl.edit);
+
+router.put("/:id", appointmentCtrl.update);
 
 module.exports = router;
