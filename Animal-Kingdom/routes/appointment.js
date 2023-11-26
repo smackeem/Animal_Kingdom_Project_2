8d1c35f37871f9b1d3fa97ca5f2945794dc83ff0
smackeem <smackeem@outlook.com>
2023-11-25 19:46:05 -0500

    Create routes to render and book an appointment.

--- Animal-Kingdom/routes/appointment.js
+++ Animal-Kingdom/routes/appointment.js
@@ -7,8 +7,12 @@ router.get('/user/:id/appointments/new', appointmentCtrl.new);
 
 router.get('/user/:id/appointments', appointmentCtrl.index);
 
+router.get('/user/:userId/appointments/:id/book', appointmentCtrl.petAppt)
+
 router.post('/user/:id/appointments', appointmentCtrl.create);
 
 router.delete('/user/:userId/appointments/:id', appointmentCtrl.delete);
+
+router.put('/user/:userId/appointments/:id', appointmentCtrl.book);
 
 module.exports = router;
