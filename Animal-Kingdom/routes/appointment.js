2a629e6b3e1b6121c1bcdc57d8a5b8851de46194
smackeem <smackeem@outlook.com>
2023-11-25 19:14:55 -0500

    Create route for veterinarian to delete an appointment.

--- Animal-Kingdom/routes/appointment.js
+++ Animal-Kingdom/routes/appointment.js
@@ -9,5 +9,6 @@ router.get('/user/:id/appointments', appointmentCtrl.index);
 
 router.post('/user/:id/appointments', appointmentCtrl.create);
 
+router.delete('/user/:userId/appointments/:id', appointmentCtrl.delete);
 
 module.exports = router;
