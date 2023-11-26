8d877a7bec2c88aec14abc58f4b0aa59d77e3384
smackeem <smackeem@outlook.com>
2023-11-26 17:51:29 -0500

    Handled conflicts.

--- Animal-Kingdom/controllers/appointment.js
+++ Animal-Kingdom/controllers/appointment.js
@@ -1,7 +1,7 @@
-const Record = require("../models/record");
-const Pet = require("../models/pet");
-const User = require("../models/users");
-const Appointment = require("../models/appointment");
+const Appointment = require('../models/appointment');
+const Record = require('../models/record');
+const Pet = require('../models/pet');
+const User = require('../models/users');
 
 module.exports = {
     new: newAppt,
