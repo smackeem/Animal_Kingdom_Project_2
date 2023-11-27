842d630b5649743f61f5d84cda85dd82ccc0b654
aobarbieri <aobarbieri@gmail.com>
2023-11-27 08:51:03 -0700

    Implemented Popup for adding a pet

--- Animal-Kingdom/public/scripts/functions.js
+++ Animal-Kingdom/public/scripts/functions.js
@@ -1,9 +1,16 @@
-// partials/header.ejs
 const avatar = document.querySelector('.avatar')
 const dropdown = document.querySelector('.nav-user ul')
+const addPet = document.getElementById('add-pet')
+const popup = document.getElementById('popup')
 
+// partials/header.ejs
 avatar.addEventListener('click', () => {
 	dropdown.classList.toggle('hidden')
+})
+
+// users/show.ejs
+addPet.addEventListener('click', () => {
+	popup.classList.remove('hidden')
 })
 
 // users/signup.ejs
