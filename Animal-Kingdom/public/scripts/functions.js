76bdc663f926c364ef195e6e7a8629e88d577236
aobarbieri <aobarbieri@gmail.com>
2023-11-26 17:53:33 -0700

    Move toggleSpecializationField function to public/scripts/functions.js, add new elements into long and signup pages to work with styles

--- Animal-Kingdom/public/scripts/functions.js
+++ Animal-Kingdom/public/scripts/functions.js
@@ -5,3 +5,14 @@ const dropdown = document.querySelector('.nav-user ul')
 avatar.addEventListener('click', () => {
 	dropdown.classList.toggle('hidden')
 })
+
+// users/signup.ejs
+function toggleSpecializationField() {
+	const userType = document.querySelector('#userType').value
+	const specField = document.querySelector('.specialization-field')
+	if (userType === 'Veterinarian') {
+		specField.style.display = 'block'
+	} else {
+		specField.style.display = 'none'
+	}
+}
