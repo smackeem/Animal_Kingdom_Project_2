3b4c177029d4c017d633abcb79a500ed69b830d1
smackeem <smackeem@outlook.com>
2023-11-28 16:45:32 -0500

    Add error handling to controllers/user.js as well as signup.ejs.

--- Animal-Kingdom/controllers/users.js
+++ Animal-Kingdom/controllers/users.js
@@ -14,49 +14,47 @@ module.exports = {
 };
 
 async function create(req, res, next) {
-  const newUser = await User.create(req.body);
-  newUser.address = req.body; // Corrected address assignment
-  console.log(newUser);
   try {
+    const { username } = req.body;
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) return res.status(500).render("users/signup", {title: "Sign up", errMsg: "Username already exists. Please choose a new one."});
+    const newUser = await User.create(req.body);
+    newUser.address = req.body; // Corrected address assignment
     await newUser.save().then((newUser) => {
-        const token = jwt.sign({ _id: newUser._id }, process.env.SECRET, {
-          expiresIn: "60 days",
-        });
+        const token = jwt.sign({ _id: newUser._id }, process.env.SECRET, {expiresIn: "60 days"});
         res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
-        res.redirect("/user/login");
-        return;
+        return res.redirect("/user/login");
       }).catch((error) => {
         console.log(error); // Log the error for debugging
-        res.status(500).send("Error saving user: " + error.message);
+        res.status(500).render("users/signup", { errMsg: error.message });
       });
   } catch (error) {
     console.log(error); // Log the error for debugging
-    res.status(500).send("Error saving user: " + error.message);
+    res.status(500).render("users/signup", { errMs: error.message });
   }
 }
 
 async function login(req, res) {
   try {
-    const user = await User.findOne({ username: req.body.username }).exec();
-    if (!user) {
-      res.render("users/login", { errMsg: "Invalid username" });
-      return;
-    }
+    //const user =
+    await User.findOne({ username: req.body.username }).then((user) => {
+      if (!user) {
+        return res.status(500).render("users/login", { errMsg: "Invalid username" });
+      }
 
-    const isMatch = await bcrypt.compare(req.body.password, user.password);
-    if (!isMatch) {
-      res.render("users/login", { errMsg: "Invalid password" });
-      return;
-    }
+      const isMatch = bcrypt.compare(req.body.password, user.password);
+      if (!isMatch) {
+        return res.status(500).render("users/login", { errMsg: "Invalid password" });
+      }
 
-    const token = jwt.sign({ _id: user._id }, process.env.SECRET, {
-      expiresIn: "60 days",
+      const token = jwt.sign({ _id: user._id }, process.env.SECRET, {expiresIn: "60 days"});
+      res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+      res.redirect(`/user/${user._id}`);
     });
-    res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
-    res.redirect(`/user/${user._id}`);
   } catch (error) {
     console.error("Login error:", error);
-    res.status(500).send(error.message);
+    res.status(500).render("users/login", { errMsg: "Error Loading Your Profile" });
   }
 }
 
@@ -64,9 +62,7 @@ async function show(req, res, next) {
   try {
     const user = await User.findById(req.params.id);
     const pets = await Pet.find({ owner: { _id: user._id } }).populate("owner");
-    const vetRecords = await Record.find({ vet: { _id: user._id } }).populate(
-      "pet"
-    );
+    const vetRecords = await Record.find({ vet: { _id: user._id } }).populate("pet");
     const allPatients = vetRecords.map((record) => record.pet);
     const patients = mergeDuplicates(allPatients, ["_id"]);
     const petAppointments = await Appointment.find({
@@ -84,6 +80,7 @@ async function show(req, res, next) {
       .populate("pet")
       .sort({ date: 1 });
     res.render("users/show", {
+      errMsg: "",
       title: `${user.username} Profile`,
       user,
       pets,
