ee668d67b6534b55fafda785d0c33ae2ee2bb5c0
aobarbieri <aobarbieri@gmail.com>
2023-11-30 08:24:15 -0700

    WIP scratchs and notes on  how to create a confirm deletion popup

--- Animal-Kingdom/public/scripts/petOwnerPopups.js
+++ Animal-Kingdom/public/scripts/petOwnerPopups.js
@@ -1,6 +1,13 @@
 const addPet = document.querySelector('.addpet')
 const newPetPopup = document.querySelector('#new-pet-popup')
 const newPetCloseBtn = document.querySelector('#new-pet-close-btn')
+const deletePet = document.querySelectorAll('.petDelete')
+const deletePetPopup = document.querySelectorAll('.petDeletePopup')
+const deletePetCloseBtn = document.querySelectorAll('.pet-delete-cancel')
+
+// console.log(deletePet)
+// console.log(deletePetPopup)
+// console.log(deletePetCloseBtn)
 
 addPet.addEventListener('click', () => {
 	newPetPopup.classList.remove('hidden')
@@ -8,3 +15,55 @@ addPet.addEventListener('click', () => {
 newPetCloseBtn.addEventListener('click', () => {
 	newPetPopup.classList.add('hidden')
 })
+
+// Get the card > to get hidden added and removed -> deletePetPopup
+// Get the delete button > to remove hidden from the card -> deletePet
+// get the cancel button > to add hidden to the card -> deletePetCloseBtn
+
+const petList = []
+const cardList = []
+
+let i = 0
+
+deletePet.forEach((p) => {
+	
+	p.addEventListener('click', () => {
+		const pet = {
+			selected: p,
+			compare: i,
+			//idx: e.pointerId
+		}
+		const card = {
+			selected: deletePetPopup[i],
+			compare: i,
+			//idx: e.pointerId
+		}
+		petList.push(pet)
+		cardList.push(card)
+		console.log(petList)
+		i++
+		if (petList[i].compare === cardList[i].compare) {
+			cardList[i].classList.remove('hidden')
+		}
+
+		// console.log(petList)
+		// console.log(cardList)
+		for (let i = 0; i < petList.length; i++){
+			console.log(petList[i])
+			console.log(cardList[i])
+		}
+	})
+})
+
+/*
+1. Get the index of the deletePet X button
+2. get the index of the deletePetPopup card
+3. compare if they are the same
+4. if yes, remove the hidden clas from the card
+*/
+
+deletePetCloseBtn.forEach((pet) => {
+	// pet.addEventListener('click', handlePetCloseBtn)
+})
+
+
