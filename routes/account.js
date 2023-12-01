90c06b326f335a702f498887fcfbd1995cd2c3fc
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-01 10:10:30 -0500

    removed uneeded comments and formatted routes

--- routes/account.js
+++ routes/account.js
@@ -1,11 +1,10 @@
 //routes/account.js
-
 const express = require("express");
 const router = express.Router();
 const accountCtrl = require("../controllers/account");
 
-router.get("/user/:id/myaccount", accountCtrl.getAccount); // get account
+router.get("/user/:id/myaccount", accountCtrl.getAccount);
 
-router.put("/user/:id/myaccount", accountCtrl.updateAccount); // update account
+router.put("/user/:id/myaccount", accountCtrl.updateAccount);
 
 module.exports = router;
--- routes/pet.js
+++ routes/pet.js
@@ -1,3 +1,4 @@
+//routes/pet.js
 const express = require("express");
 const router = express.Router();
 
