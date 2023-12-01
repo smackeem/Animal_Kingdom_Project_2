eac1261122793a59ccfb7e151a7c421f94742ca9
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-01 10:17:58 -0500

    format and clean comments in database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
-//config/database.js
+// config/database.js
 // import mongoose library
 const mongoose = require("mongoose");
 
-//storing connection string in a variable - for testing
+// storing connection string in a variable - for testing
 const connectionString = process.env.DATABASE_URL;
 
 //  establish connection to database
@@ -16,7 +16,7 @@ db.on("connected", function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
 
-//connection event listener
+// connection event listener
 db.on("error", function (error) {
   console.log(`something went WRONG with the connection`);
 });
